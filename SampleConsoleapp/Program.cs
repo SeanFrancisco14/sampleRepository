011dd2274028d874a6e6d7a2b2a9c771d7283407
Grace Paclibar <graceannruthnpaclibar@iskolarngbayan.pup.edu.ph>
2022-04-25 20:53:07 +0800

    Added Places

--- SampleConsoleapp/Program.cs
+++ SampleConsoleapp/Program.cs
@@ -7,7 +7,9 @@ namespace SampleConsoleapp
         static void Main(string[] args)
         {
             string[] Names = {"John", "Raye", "Mike", "Pam", "Carlo", "Imogen", "Caleb"};
+            string[] Places = { "Manila", "Cebu", "Davao", "Pasay" };
 
+            Console.WriteLine(Places[0]);
             Console.WriteLine(Names[1]);
             Console.ReadLine();
         }
