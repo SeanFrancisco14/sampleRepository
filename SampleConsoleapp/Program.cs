7405838fdfda08e04ccb468581bf9b1d9f0580b8
Grace Paclibar <graceannruthnpaclibar@iskolarngbayan.pup.edu.ph>
2022-04-25 21:08:51 +0800

    Added [Name] at [Place]

--- SampleConsoleapp/Program.cs
+++ SampleConsoleapp/Program.cs
@@ -9,8 +9,14 @@ namespace SampleConsoleapp
             string[] Names = {"John", "Raye", "Mike", "Pam", "Carlo", "Imogen", "Caleb"};
             string[] Places = { "Manila", "Cebu", "Davao", "Pasay" };
 
+            Random rndNames = new Random();
+            int indexNames = rndNames.Next(Names.Length);
+            Random rndPlaces = new Random();
+            int indexPlaces = rndPlaces.Next(Places.Length);
+
             Console.WriteLine(Places[0]);
             Console.WriteLine(Names[1]);
+            Console.WriteLine($"{Names[indexNames]} is at {Places[indexPlaces]}");
             Console.ReadLine();
         }
     }
