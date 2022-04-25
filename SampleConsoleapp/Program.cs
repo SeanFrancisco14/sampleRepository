ad8fdae47f6e9bf4ff81d38448c06d6429197a84
Grace Paclibar <graceannruthnpaclibar@iskolarngbayan.pup.edu.ph>
2022-04-25 15:40:00 +0800

    Added 2 names

--- SampleConsoleapp/Program.cs
+++ SampleConsoleapp/Program.cs
@@ -6,7 +6,7 @@ namespace SampleConsoleapp
     {
         static void Main(string[] args)
         {
-            string[] Names = {"John", "Raye", "Mike", "Pam", "Carlo"};
+            string[] Names = {"John", "Raye", "Mike", "Pam", "Carlo", "Imogen", "Caleb"};
 
             Console.WriteLine(Names[1]);
             Console.ReadLine();
